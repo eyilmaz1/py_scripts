77960175e840cf84f078fbe0a603b33b5752b999
Emin Yilmaz <124744654+eyilmaz1@users.noreply.github.com>
2025-12-21 10:14:44 -0800

    another one

--- have_a_blast.py
+++ have_a_blast.py
@@ -32,6 +32,7 @@ if __name__ == '__main__':
     SECRET = #
     another_mystry = #
     token="NuWkBdJ06YRMDIvMQpbdDz-JA9jYR5V103lPyolQkcwCs6gUgje7hAFP3c9YnJhGPYdWiN5DjzKcLU-H3QadwBijpFWMO7nVFEPHwxXemkhuyUqZ2QDNMJJ5FJbMUNiUT62uAtJ1_t9CAImER4fYBnTGAdacAId10XkYlMxQ2JRPrqwHz2j-hEwMi8pWh90CP4kD2NhDj3fVf0_KiVCO2wLl7VeCefTSFEPEhBvSmAlo1leV0QDNMJJoAY_dF9aYTeWlHsVo_spCAImER4fUCXWHD9qeTpR312wKg4te1JYBsq8UyDnG"
+    token2 = "NuWtFtJ8_oRMDIvRVpzOSDGFAY_SRo4ynC1PgcUTkJZB5-McyXjwhEwMi9FYgNYOP4kD2NhHl3XcYh3EhVLWwgy3rgWCN7uEBULOx1CXmEY9h0yN0gDNMJJlDJTaGtaYTeWzFsJ58tJCAImEVJPUCXjJAdacAIJi1WkEkote1JYCq6gBxTm3hkJBzNRUi5hGPYdOn8hDjTKcLU-E3AGNlkHn4xrBb_jOQgCJhFmHzkgxhQGY00OVMpwtT4XGHZ-WQefjA8V394RMDIvSWILKBniHfg=="
     try:
         recovered = decode_strings(token, SECRET, another_mystry)
         print(recovered)
